ee5cfb42450efd702d23d9c1c5d1f2b6fbed4dd8
Aaron Chesley <FlashBanistan66@gmail.com>
2020-12-16 23:05:57 -0700

    wire up resume state

--- jobber.js/src/app/resume/resume.module.ts
+++ jobber.js/src/app/resume/resume.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { StoreModule } from "@ngrx/store";
+import { EffectsModule } from "@ngrx/effects";
+import { resumeReducers } from "./store/resume-reducers";
+import { ResumeEffects } from "./store/resume-effects";
 import { ResumeRoutingModule } from "./resume-routing.module";
 import { ResumeLayoutComponent } from "./resume-layout/resume-layout.component";
 import { MaterialModule } from "../shared/material.module";
 import { ContactInformationModule } from "./contact-information/contact-information.module";
 import { ResumeSectionComponent } from "./resume-section/resume-section.component";
-import { ResumeSectionHeaderComponent } from './resume-section/resume-section-header/resume-section-header.component';
-import { ResumeSectionIconComponent } from './resume-section/resume-section-icon/resume-section-icon.component';
-import { ResumeSectionContentComponent } from './resume-section/resume-section-content/resume-section-content.component';
+import { ResumeSectionHeaderComponent } from "./resume-section/resume-section-header/resume-section-header.component";
+import { ResumeSectionIconComponent } from "./resume-section/resume-section-icon/resume-section-icon.component";
+import { ResumeSectionContentComponent } from "./resume-section/resume-section-content/resume-section-content.component";
 
 @NgModule({
-  declarations: [ResumeLayoutComponent, ResumeSectionComponent, ResumeSectionHeaderComponent, ResumeSectionIconComponent, ResumeSectionContentComponent],
+  declarations: [
+    ResumeLayoutComponent,
+    ResumeSectionComponent,
+    ResumeSectionHeaderComponent,
+    ResumeSectionIconComponent,
+    ResumeSectionContentComponent,
+  ],
   imports: [
     CommonModule,
+    StoreModule.forFeature("resume", resumeReducers),
+    EffectsModule.forFeature([ResumeEffects]),
     MaterialModule,
     ResumeRoutingModule,
     ContactInformationModule,
--- jobber.js/src/app/resume/store/resume-state.ts
+++ jobber.js/src/app/resume/store/resume-state.ts
@@ -1,7 +1,11 @@
+import { Resume } from "../interfaces/resume.interface";
+
 export interface ResumeState {
+  resume: Resume;
   isLoading: boolean;
 }
 
 export const initialResumeState: ResumeState = {
+  resume: null,
   isLoading: false,
 };
