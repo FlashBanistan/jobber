14db287d6577db5ffaef0d9d34604a364d44aa99
Aaron Chesley <FlashBanistan66@gmail.com>
2020-12-18 14:34:03 -0700

    wire up contact information

--- jobber.js/src/app/resume/contact-information/contact-information-container.component.ts
+++ jobber.js/src/app/resume/contact-information/contact-information-container.component.ts
@@ -1,22 +1,59 @@
 import { Component, ChangeDetectionStrategy } from "@angular/core";
 import { Store } from "@ngrx/store";
 import { Observable } from "rxjs";
-import { AppState } from "src/app/store/state/state";
-import { Resume } from "../interfaces/resume.interface";
+import { OnlineProfile } from "../interfaces/online-profile.interface";
+import { ResumeLocation } from "../interfaces/resume-location.interface";
+import {
+  selectEmail,
+  selectLabel,
+  selectLocation,
+  selectName,
+  selectPhone,
+  selectPicture,
+  selectProfiles,
+  selectSummary,
+  selectWebsite,
+} from "../store/resume-selectors";
+import { ResumeState } from "../store/resume-state";
 
 @Component({
   selector: "app-contact-information-container",
   template: `
     <app-contact-information
-      [resume]="resume$ | async"
-    ></app-contact-information>
+      [name]="name$ | async"
+      [label]="label$ | async"
+      [picture]="picture$ | async"
+      [email]="email$ | async"
+      [phone]="phone$ | async"
+      [website]="website$ | async"
+      [summary]="summary$ | async"
+      [location]="location$ | async"
+      [profiles]="profiles$ | async"
+    >
+    </app-contact-information>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ContactInformationContainerComponent {
-  resume$: Observable<Resume>;
+  name$: Observable<string>;
+  label$: Observable<string>;
+  picture$: Observable<string>;
+  email$: Observable<string>;
+  phone$: Observable<string>;
+  website$: Observable<string>;
+  summary$: Observable<string>;
+  location$: Observable<ResumeLocation>;
+  profiles$: Observable<OnlineProfile[]>;
 
-  constructor(private store: Store<AppState>) {
-    // this.resume$ = this.store.select(selectResume);
+  constructor(private store: Store<ResumeState>) {
+    this.name$ = this.store.select(selectName);
+    this.label$ = this.store.select(selectLabel);
+    this.picture$ = this.store.select(selectPicture);
+    this.email$ = this.store.select(selectEmail);
+    this.phone$ = this.store.select(selectPhone);
+    this.website$ = this.store.select(selectWebsite);
+    this.summary$ = this.store.select(selectSummary);
+    this.location$ = this.store.select(selectLocation);
+    this.profiles$ = this.store.select(selectProfiles);
   }
 }
--- jobber.js/src/app/resume/contact-information/contact-information.component.ts
+++ jobber.js/src/app/resume/contact-information/contact-information.component.ts
@@ -1,15 +1,21 @@
-import { Component, Input, OnInit } from "@angular/core";
-import { Resume } from "../interfaces/resume.interface";
+import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
+import { OnlineProfile } from "../interfaces/online-profile.interface";
+import { ResumeLocation } from "../interfaces/resume-location.interface";
 
 @Component({
   selector: "app-contact-information",
   templateUrl: "./contact-information.component.html",
   styleUrls: ["./contact-information.component.scss"],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ContactInformationComponent implements OnInit {
-  @Input() resume: Resume;
-
-  constructor() {}
-
-  ngOnInit() {}
+export class ContactInformationComponent {
+  @Input() name: string;
+  @Input() label: string;
+  @Input() picture: string;
+  @Input() email: string;
+  @Input() phone: string;
+  @Input() website: string;
+  @Input() summary: string;
+  @Input() location: ResumeLocation;
+  @Input() profiles: OnlineProfile[];
 }
