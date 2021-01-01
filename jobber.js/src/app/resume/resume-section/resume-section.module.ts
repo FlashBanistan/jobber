5bd8bf8665d01c44ca5f1d1a57f9b9f00394919a
Aaron Chesley <FlashBanistan66@gmail.com>
2020-12-31 22:17:44 -0700

    boilerplate

--- jobber.js/src/app/resume/resume-section/resume-section.module.ts
+++ jobber.js/src/app/resume/resume-section/resume-section.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from "@angular/core";
 import { ResumeSectionContentComponent } from "./resume-section-content/resume-section-content.component";
 import { ResumeSectionHeaderComponent } from "./resume-section-header/resume-section-header.component";
 import { ResumeSectionIconComponent } from "./resume-section-icon/resume-section-icon.component";
-import { ResumeSectionComponent } from "./resume-section.component";
+import { ResumeSectionComponent } from "./resume-section/resume-section.component";
 
 @NgModule({
   declarations: [
